57897e124ca1d438f5d7edb213e4877614707216
rajdeep umrani <umranirajdeep13@gmail.com>
2025-07-29 03:26:16 -0400

    Hyperparameter Tuning Done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -14,8 +14,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
-from src.utils import save_object
-from src.utils import evaluate_models
+from src.utils import save_object, evaluate_models  # Import evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -24,7 +23,7 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
-        self.best_model = None
+        self.best_model_obj = None
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -44,32 +43,62 @@ class ModelTrainer:
                 "XGBoost Regressor": XGBRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boost ": GradientBoostingRegressor(),
+            }
 
+            params = {
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {}, # Often no hyperparameters for Linear Regression in a simple case
+                "KNN Nearest": {
+                    'n_neighbors': [5, 7, 9, 11]
+                },
+                "Adaboost Regressor": {
+                    'n_estimators': [50, 100, 200],  # Example hyperparameters
+                    'learning_rate': [0.01, 0.05, 0.1]
+                },
+                "Catboost Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "XGBoost Regressor": {
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'max_depth': [3, 5, 7]
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter': ['best', 'random'],
+                    'max_features': [10,20,30,50,75,100]
+                },
+                "Gradient Boost ": {
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'max_depth': [3, 5, 7]
                 }
-            model_report : dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models)
+            }
+
+
+            model_report : dict = evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test, models = models, params = params)
 
             best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
 
-            self.best_model = models[best_model_name]
+            self.best_model_obj = models[best_model_name]
 
-            if best_model_score<0.6:
-                raise CustomException("No Best Model Found")
-            logging.info("Best found model on both training and testing dataset")
+            if best_model_score < 0.6:
+                raise CustomException("No best model found with R2 score greater than 0.6")
+
+            logging.info(f"Best model found: {best_model_name} with R2 score: {best_model_score}")
+
             save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj = self.best_model
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=self.best_model_obj
             )
 
-            predicted = self.best_model.predict(X_test)
-
-            r2_sqaure = r2_score(y_test, predicted)
-            return r2_sqaure
+            return best_model_score
 
         except Exception as e:
-            raise CustomException(e,sys)
-
-
-
-
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -18,13 +19,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train, X_test, y_test, models):
+def evaluate_models(X_train,y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -36,6 +42,8 @@ def evaluate_models(X_train,y_train, X_test, y_test, models):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
+
+
 
         return report
         
